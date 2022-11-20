33452e58b3a2c85cac42b27d2dcd3de2c789952a
KIM <kms000229@gmail.com>
2022-11-20 21:59:13 +0900

    Update todo.js
    +comment

--- JS/todo.js
+++ JS/todo.js
@@ -4,15 +4,15 @@ const toDoList = document.querySelector(".todo-list");
 
 const TODO_KEY = "toDos";
 
-let toDos = [];
+let toDos = []; //배열이 비어있기때문에 페이지 리로드시 새로 입력되는값에 덮여짐. 45번줄 확인.
 
 function saveToDos() {
-  localStorage.setItem(TODO_KEY, JSON.stringify(toDos)); //toDos Array에 들어가는 값음 "값"의 형태로 들어가게 하기위함.
+  localStorage.setItem(TODO_KEY, JSON.stringify(toDos)); //toDos Array에 들어가는 값음 "값"(text)의 형태로 들어가게 하기위함.
 }
 
-//deleteToDo()로 하고, const deleteList = this.parentElement; 로 해도 동작함
+//deleteToDo()로 하고, const deleteList = this.parentElement; 로 해도 동작함 -> why?
 function deleteToDo(event) {
-  const deleteList = event.target.parentElement;
+  const deleteList = event.target.parentElement; // 삭제하려는 내용이 담긴 list가 button(and span)의 부모엘리먼트 이므로
   deleteList.remove();
 }
 
@@ -30,7 +30,7 @@ function paintToDo(newTodo) {
 function toDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
-  toDoInput.value = ""; //인풋텍스트를 받은 후 비워내기위함, newTodo와는 별개
+  toDoInput.value = ""; //인풋텍스트를 받은 후 비워내기위함, 윗줄의 newTodo와는 별개
   toDos.push(newTodo);
   paintToDo(newTodo);
   saveToDos();
@@ -41,7 +41,7 @@ toDoForm.addEventListener("submit", toDoSubmit);
 const savedToDos = localStorage.getItem(TODO_KEY); // "[]" 의 형태로 get해옴.
 
 if (savedToDos) {
-  const parsedToDos = JSON.parse(savedToDos); // "[]" -> [] 배열이 될것.
-  toDos = parsedToDos;
+  const parsedToDos = JSON.parse(savedToDos); // "[]" -> [] = 변환하여 배열로 만듬.
+  toDos = parsedToDos; //페이지 리로드 시 덮어쓰기 방지를 위해 리로드 전 배열을 현재배열로 복사함
   parsedToDos.forEach(paintToDo);
 }
