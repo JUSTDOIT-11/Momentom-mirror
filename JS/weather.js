a79ac3558cf594c1a1b7912946abdbae346c2a94
KIM <kms000229@gmail.com>
2022-11-22 19:19:50 +0900

    using-API(GEOLOCATION)
    api를 사용하여 위치, 날씨 등을 출력하는 방법 공부

---
+++ JS/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "fd3fcc2a19edc0fb2ce2255ab05109f2";
+function geoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const showCity = document.querySelector(".weather span:first-child");
+      const showWeather = document.querySelector(".weather span:nth-child(2)");
+      const showTemp = document.querySelector(".weather span:last-child");
+      showCity.innerText = data.name;
+      showWeather.innerText = data.weather[0].main;
+      showTemp.innerText = data.main.temp;
+    });
+}
+
+function geoErr() {
+  alert("Can't get your Position ㅠㅜ");
+}
+
+navigator.geolocation.getCurrentPosition(geoOk, geoErr);
