0e50db4aa63ebb456fdeac99fed0f7d35cdb30bb
KIM <kms000229@gmail.com>
2022-12-01 22:20:15 +0900

    +cog-animation
    무슨 설정켜는 애니메이션하나 만들기가 이렇게 어렵냐....

--- JS/option.js
+++ JS/option.js
@@ -1,8 +1,37 @@
 const cog = document.querySelector(".cog"); // 코그 이모티콘
 const arrowBox = document.querySelector(".arrow-box"); // 말풍선 모양 박스
 
+let clickCount = 0;
+
+const cogopenSpinning = [
+  { transform: "rotate(0deg) scale(1)" },
+  { transform: "rotate(90deg) scale(1.2)" },
+];
+
+const cogcloseSpinning = [
+  { transform: "rotate(90deg) scale(1.2)" },
+  { transform: "rotate(0deg) scale(1)" },
+];
+
+const cogTiming = {
+  duration: 500,
+  fill: "forwards",
+  easing: "ease-in-out",
+};
+
+//야매 코그 애니메이션...
+function cogAnimation() {
+  if (clickCount % 2 === 0) {
+    cog.animate(cogopenSpinning, cogTiming);
+  } else if (clickCount % 2 === 1) {
+    cog.animate(cogcloseSpinning, cogTiming);
+  }
+}
+
 function openOption() {
   arrowBox.classList.toggle("hidden");
+  cogAnimation();
+  clickCount++;
 }
 
 cog.addEventListener("click", openOption);
