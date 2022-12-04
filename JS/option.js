c2df2c9c24b1b770b93832e0ab4905da0e5ef013
KIM <kms000229@gmail.com>
2022-12-04 14:34:59 +0900

    ++optionCSS-end
    스위치에 따라 켜지고 꺼지는 동작 js로 만듦.
    -새로고침 시 상태 유지는 못만듬..ㅠ

--- JS/option.js
+++ JS/option.js
@@ -1,5 +1,15 @@
 const cog = document.querySelector(".cog"); // 코그 이모티콘
 const arrowBox = document.querySelector(".arrow-box"); // 말풍선 모양 박스
+const quotesSwitch = document.querySelector(".Quotes-switch .switch"); //명언 label checkbox
+const quotesBox = document.querySelector(".quote"); //명언 div박스
+const clockSwitch = document.querySelector(".Clock-switch .switch"); //시계 label checkbox
+//const clock = document.querySelector(".clock"); //시계 h2
+const todoSwitch = document.querySelector(".Todo-switch .switch"); //할일 label checkbox
+//const toDoForm = document.querySelector(".todo-form"); //할일 form
+//const toDoList = document.querySelector(".todo-list"); //할일 리스트
+const greedingSwitch = document.querySelector(".Greeting-switch .switch"); //환영인사 label checkbox
+//const greeding = document.querySelector("#greeding"); //환영인사 h1
+//const loginForm = document.querySelector(".login-form"); //로그인 form
 
 let clickCount = 0;
 
@@ -32,6 +42,7 @@ function cogAnimation() {
   }
 }
 
+//flexbox 안쓸때 toggle 사용하는 방법 남겨두려고 남긴 함수
 function openOption() {
   //arrowBox.classList.toggle("hidden");
   cogAnimation();
@@ -39,3 +50,51 @@ function openOption() {
 }
 
 cog.addEventListener("click", openOption);
+
+//quotes on-of 스위치
+function quotesOnOff() {
+  if (this.checked) {
+    quotesBox.removeAttribute("hidden", "");
+  } else {
+    quotesBox.setAttribute("hidden", "");
+  }
+}
+
+quotesSwitch.addEventListener("change", quotesOnOff);
+
+//clock on-of 스위치
+function clockOnOff() {
+  if (this.checked) {
+    clock.removeAttribute("hidden", "");
+  } else {
+    clock.setAttribute("hidden", "");
+  }
+}
+
+clockSwitch.addEventListener("change", clockOnOff);
+
+//todo on-of 스위치
+function toDoOnOff() {
+  if (this.checked) {
+    toDoForm.removeAttribute("hidden", "");
+    toDoList.removeAttribute("hidden", "");
+  } else {
+    toDoForm.setAttribute("hidden", "");
+    toDoList.setAttribute("hidden", "");
+  }
+}
+
+todoSwitch.addEventListener("change", toDoOnOff);
+
+//greeding on-of 스위치
+function greedingOnOff() {
+  if (this.checked) {
+    greeding.removeAttribute("hidden", "");
+    loginForm.removeAttribute("hidden", "");
+  } else {
+    greeding.setAttribute("hidden", "");
+    loginForm.setAttribute("hidden", "");
+  }
+}
+
+greedingSwitch.addEventListener("change", greedingOnOff);
